ada716e03819a0a007c49fc8ef08603ca84bdbe7
lighter <apple.xva@gmail.com>
2020-03-07 12:19:28 +0800

    Initial Commit

---
+++ StatusBar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  StatusBar
+//
+//  Created by lighter on 2020/3/7.
+//  Copyright © 2020 lighter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

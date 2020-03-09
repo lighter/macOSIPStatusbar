eeca1002f6576dc1f8af949fc9fdae91c0ff80dd
lighter <apple.xva@gmail.com>
2020-03-09 16:30:25 +0800

    get ip by call api

--- StatusBar/AppDelegate.swift
+++ StatusBar/AppDelegate.swift
@@ -8,32 +8,63 @@
 
 import Cocoa
 import SwiftUI
+import NetworkExtension
+
+struct myIPResponse: Codable {
+    let ip: String?
+    let country: String?
+    let cc: String?
+    
+    init(ip: String? = nil, country: String? = nil, cc: String? = nil) {
+        self.ip = ip
+        self.country = country
+        self.cc = cc
+    }
+}
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
-
+    let statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        // The application does not appear in the Dock and may not create
+        // windows or be activated.
+        NSApp.setActivationPolicy(.prohibited)
 
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        self.start()
+        
+        let halfHour = 30 * 60
+        Timer.scheduledTimer(withTimeInterval: TimeInterval(halfHour), repeats: true) { timer in
+            self.start()
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func start() {
+        self.getMyIP() { (output) in
+            DispatchQueue.main.async {
+                self.statusBarItem.button?.title = "\(output.ip!), \(output.cc!)"
+            }
+        }
+    }
+    
+    func getMyIP(completionBlock: @escaping (myIPResponse) -> Void) {
+        let urlString = "https://api.myip.com/"
+        if let url = URL(string: urlString) {
+            URLSession.shared.dataTask(with: url) { data, res, err in
+                if let data = data {
+                    let decoder = JSONDecoder()
+                    if let json = try? decoder.decode(myIPResponse.self, from: data) {
+                        completionBlock(json)
+                    }
+                }
+            }.resume()
+        }
+    }
 }
 
